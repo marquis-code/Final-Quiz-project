7da09ae32d1678959ed3da1e5c0c54d0d35bdafe
Abah Marquis <abahmarquis@gmail.com>
2020-10-30 01:19:41 +0100

    version

--- server.js
+++ server.js
@@ -61,10 +61,10 @@ app.use("/quiz", quizRouter);
 
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
